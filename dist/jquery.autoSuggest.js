hlsolutions/jquery-autosuggest

commit c7a79fe7623d097f28a6b76f793d4bc1e12c78aa

Allows selection by hitting ENTER if neverSubmit is true

dist/jquery.autoSuggest.js
@@ -1,4 +1,4 @@
-/*! jQuery AutoSuggest - v2.3.0 - 2013-03-29
+/*! jQuery AutoSuggest - v2.3.0 - 2013-08-05
  * URL: http://hlsolutions.github.com/jquery-autosuggest
  * Copyright (c) 2013 Jan Philipp
  * Licensed MIT, GPL */
@@ -22,8 +22,7 @@ Based on the 1.6er release dated in July, 2012
 
   $ = jQuery;
 
-  /* A collection of utility functions.
-  */
+  /* A collection of utility functions.*/
 
 
   Utils = (function() {
@@ -43,7 +42,6 @@ Based on the 1.6er release dated in July, 2012
 
     Utils.setPlaceholderEnabled = function(input, enable) {
       var from, targets, to;
-
       targets = ['placeholder', 'disabled-placeholder'];
       if (enable) {
         from = targets[1];
@@ -65,8 +63,7 @@ Based on the 1.6er release dated in July, 2012
 
   })();
 
-  /* A collection of configuration resolvers.
-  */
+  /* A collection of configuration resolvers.*/
 
 
   ConfigResolver = (function() {
@@ -82,7 +79,6 @@ Based on the 1.6er release dated in July, 2012
 
     ConfigResolver.getExtraParams = function(options) {
       var obj, pair, parts, result, _i, _len, _ref;
-
       result = options.extraParams;
       if ($.isFunction(result)) {
         result = result(this);
@@ -114,8 +110,7 @@ Based on the 1.6er release dated in July, 2012
 
   })();
 
-  /* The SelectionControl maintains and manage any selections.
-  */
+  /* The SelectionControl maintains and manage any selections.*/
 
 
   SelectionHolder = (function() {
@@ -132,7 +127,6 @@ Based on the 1.6er release dated in July, 2012
 
     SelectionHolder.prototype.syncToHiddenField = function() {
       var item, value, _i, _len, _ref;
-
       if (!this.hiddenField) {
         return;
       }
@@ -188,7 +182,6 @@ Based on the 1.6er release dated in July, 2012
 
     Events.onSelectionAdd = function(scope, containerElement, detachedElement, options, item, selections) {
       var element;
-
       element = options.onSelectionAdd.call(scope, containerElement, detachedElement, options);
       Utils.setPlaceholderEnabled(scope, selections.length === 0);
       if ($.isFunction(options.afterSelectionAdd)) {
@@ -514,7 +507,6 @@ Based on the 1.6er release dated in July, 2012
   pluginMethods = {
     init: function(dataSource, options) {
       var ajaxRequest, fetcher;
-
       options = $.extend({}, defaults, options);
       ajaxRequest = null;
       if (options.remoteFilter === 'auto') {
@@ -527,10 +519,8 @@ Based on the 1.6er release dated in July, 2012
           case 'string':
             return function(query, callback) {
               var ajaxRequestConfig, extraParams, onDone, params;
-
               params = {};
-              /* ensures query is encoded
-              */
+              /* ensures query is encoded*/
 
               params["" + options.queryParam] = encodeURIComponent(decodeURIComponent(query));
               if (options.retrieveLimit) {
@@ -572,7 +562,6 @@ Based on the 1.6er release dated in July, 2012
 
       return this.each(function() {
         var abortRequest, addSelection, clonePublicApi, currentSelection, element, elementId, hiddenInputField, hiddenInputFieldId, hiddenInputFieldName, i, input, inputWrapper, input_focus, interval, item, keyChange, lastKeyPressCode, lastKeyWasTab, moveResultSelection, new_value, num_count, prev, processData, processRequest, publicApi, resultsContainer, resultsList, selectionsContainer, timeout, value, _i, _j, _len, _len1, _ref, _ref1;
-
         options.inputAttrs = $.extend(options.inputAttrs, {});
         input_focus = false;
         input = $(this);
@@ -635,7 +624,6 @@ Based on the 1.6er release dated in July, 2012
         publicApi = {
           add: function(data) {
             var counted, item;
-
             counted = $(selectionsContainer).find('li').length;
             item = addSelection(data, "u" + counted);
             if (item != null) {
@@ -667,7 +655,6 @@ Based on the 1.6er release dated in July, 2012
 
         addSelection = function(data, num) {
           var closeElement, item;
-
           currentSelection.add(data[options.selectedValuesProp]);
           item = $("<li class=\"as-selection-item\" id=\"as-selection-" + num + "\" data-value=\"" + (Utils.escapeQuotes(Utils.escapeHtml(data[options.selectedValuesProp]))) + "\"></li>");
           item.on({
@@ -759,7 +746,6 @@ Based on the 1.6er release dated in July, 2012
           */
 
           var string, _k, _results;
-
           if (lastKeyPressCode === 46 || __indexOf.call((function() {
             _results = [];
             for (_k = 9; _k < 31; _k++){ _results.push(_k); }
@@ -790,7 +776,6 @@ Based on the 1.6er release dated in July, 2012
         };
         processData = function(data, query) {
           var formatted, forward, matchCount, name, num, regex, resultsContainerVisible, str, text, workingData, _k, _l, _len2, _len3, _ref2;
-
           if (!options.matchCase) {
             query = query.toLowerCase();
           }
@@ -825,7 +810,6 @@ Based on the 1.6er release dated in July, 2012
               formatted.on({
                 click: function() {
                   var number, raw_data;
-
                   element = $(this);
                   raw_data = element.data('data');
                   number = raw_data.num;
@@ -857,8 +841,7 @@ Based on the 1.6er release dated in July, 2012
               workingData = $.extend({}, data[num]);
               query = query.replace(/"/g, '\\"');
               regex = !options.matchCase ? new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + Utils.escapeHtml(query) + ")(?![^<>]*>)(?![^&;]+;)", "gi") : new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + Utils.escapeHtml(query) + ")(?![^<>]*>)(?![^&;]+;)", "g");
-              /* When this is a string, escape the value and process a regular replacement for highlighting.
-              */
+              /* When this is a string, escape the value and process a regular replacement for highlighting.*/
 
               if (typeof workingData[options.selectedItemProp] === 'string') {
                 workingData[options.selectedItemProp] = Utils.escapeHtml(workingData[options.selectedItemProp]);
@@ -904,7 +887,6 @@ Based on the 1.6er release dated in July, 2012
         };
         moveResultSelection = function(direction) {
           var active, lis, start;
-
           if (resultsContainer.find(':visible').length) {
             lis = resultsContainer.find('li');
             switch (direction) {
@@ -982,11 +964,9 @@ Based on the 1.6er release dated in July, 2012
             Utils.setPlaceholderEnabled(element, currentSelection.isEmpty());
           },
           keydown: function(event) {
-            /* track the last key pressed
-            */
+            /* track the last key pressed*/
 
             var active, first_focus, i_input, n_data, _selection, _selections;
-
             lastKeyPressCode = event.keyCode;
             first_focus = false;
             switch (event.keyCode) {
@@ -1041,22 +1021,21 @@ Based on the 1.6er release dated in July, 2012
                 break;
               case 9:
               case 188:
+              case 13:
                 active = resultsContainer.find('li.active:visible:first');
                 if (options.canGenerateNewSelections) {
                   lastKeyWasTab = true;
                   i_input = input.val().replace(/(,)/g, '');
-                  /* Generate a new bubble with text when no suggestion selected
-                  */
+                  /* Generate a new bubble with text when no suggestion selected*/
 
-                  if (i_input !== '' && !currentSelection.exist(i_input) && i_input.length >= options.minChars && active.length === 0) {
+                  if (i_input !== '' && !currentSelection.exist(i_input) && i_input.length >= options.minChars && active.length === 0 && (options.neverSubmit || event.keyCode !== 13)) {
                     event.preventDefault();
                     n_data = {};
                     n_data["" + options.selectedItemProp] = i_input;
                     n_data["" + options.selectedValuesProp] = i_input;
                     addSelection(n_data, "00" + (selectionsContainer.find('li').length + 1));
                     input.val('');
-                    /* Cancel previous ajaxRequest when new tag is added
-                    */
+                    /* Cancel previous ajaxRequest when new tag is added*/
 
                     abortRequest();
                   }
@@ -1098,7 +1077,6 @@ Based on the 1.6er release dated in July, 2012
     },
     add: function() {
       var element, item, items, _i, _len;
-
       items = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
       element = $(this);
       for (_i = 0, _len = items.length; _i < _len; _i++) {
@@ -1108,7 +1086,6 @@ Based on the 1.6er release dated in July, 2012
     },
     remove: function() {
       var element, value, values, _i, _len;
-
       values = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
       element = $(this);
       for (_i = 0, _len = values.length; _i < _len; _i++) {

src/jquery.autoSuggest.coffee
@@ -865,14 +865,14 @@ pluginMethods =
                   keyChange()
                   return
                 ), options.keyDelay
-            when 9, 188 # tab, comma
+            when 9, 188, 13 # tab, comma, return
               active = resultsContainer.find('li.active:visible:first')
               if options.canGenerateNewSelections
                 lastKeyWasTab = true
                 # remove all comma
                 i_input = input.val().replace /(,)/g, ''
                 ### Generate a new bubble with text when no suggestion selected ###
-                if i_input isnt '' && !currentSelection.exist(i_input) && i_input.length >= options.minChars && active.length is 0
+                if i_input isnt '' && !currentSelection.exist(i_input) && i_input.length >= options.minChars && active.length is 0 && (options.neverSubmit || event.keyCode != 13)
                   event.preventDefault()
                   n_data = {}
                   n_data["#{options.selectedItemProp}"] = i_input

dist/jquery.autoSuggest.css
@@ -27,7 +27,7 @@ ul.as-selections li.as-selection-item {
   color: #2b3840;
   font-size: 13px;
   font-family: "Lucida Grande", arial, sans-serif;
-  text-shadow: 0 1px 1px white;
+  text-shadow: 0 1px 1px #fff;
   background-color: #d4eaf6;
   background: -webkit-gradient(linear, 50% 0%, 50% 100%, color-stop(0%, #d4eaf6), color-stop(100%, #bfe0f1));
   background: -webkit-linear-gradient(top, #d4eaf6, #bfe0f1);
@@ -62,7 +62,7 @@ ul.as-selections li.as-selection-item a.as-close {
   font-size: 14px;
   font-weight: bold;
   text-shadow: 0 1px 1px white;
-  -webkit-transition: color 0.1s ease-in;
+  -webkit-transition: color .1s ease-in;
 }
 ul.as-selections li.as-selection-item.blur {
   color: #666666;

test/jquery.autoSuggest_test.js
@@ -479,6 +479,28 @@
       equal(selections().length, 0, "Should remove using a callback.");
     });
 
+    module('Configuration: "options.neverSubmit"', {
+      teardown : function () {
+        remove();
+      }
+    });
+
+    test('Type "Yao Ming" and select it by ENTER', 3, function () {
+      el = create(null, $.extend({}, options, {
+        neverSubmit : true
+      }));
+      // Type "Yap Ming" and "\t"
+      el.focus();
+      el.val("Yao Ming");
+      el.simulate("keydown", {"keyCode" : keyCode.ENTER});
+
+      sel = selections();
+      equal(sel.length, 1, "Should have one name");
+      equal($(sel[0]).text(), "×Yao Ming", "Should be Yao Ming");
+      equal(value().val(), ",Yao Ming,", "Should be Yao Ming");
+      remove();
+    });
+
     module('Configuration: "onAjaxRequestAlways"', {
       teardown : function () {
         remove();

commit 45a53948b1aa7646ef73bb0df202ee7bcbf4ab00

Fixes bug introduced in c7a79fe

dist/jquery.autoSuggest.js
@@ -1,4 +1,4 @@
-/*! jQuery AutoSuggest - v2.3.0 - 2013-08-05
+/*! jQuery AutoSuggest - v2.3.0 - 2013-08-06
  * URL: http://hlsolutions.github.com/jquery-autosuggest
  * Copyright (c) 2013 Jan Philipp
  * Licensed MIT, GPL */
@@ -1044,15 +1044,6 @@ Based on the 1.6er release dated in July, 2012
                 }
                 if (active.length) {
                   lastKeyWasTab = false;
-                  active.click();
-                  resultsContainer.hide();
-                  event.preventDefault();
-                }
-                break;
-              case 13:
-                lastKeyWasTab = false;
-                active = resultsContainer.find('li.active:first');
-                if (active.length) {
                   active.click();
                   resultsContainer.hide();
                 }

src/jquery.autoSuggest.coffee
@@ -887,13 +887,6 @@ pluginMethods =
                 lastKeyWasTab = false
                 active.click()
                 resultsContainer.hide()
-                event.preventDefault()
-            when 13 # return
-              lastKeyWasTab = false
-              active = resultsContainer.find('li.active:first')
-              if active.length
-                active.click()
-                resultsContainer.hide()
               if options.neverSubmit || active.length
                 event.preventDefault()
             when 27 # esc